116926e16a5d11c6c36b64e38c3a3330f4e7fa96
Fu Yen Hsiao <fuyanshaw@gmail.com>
2018-06-09 10:23:31 -0400

    footer_nav, scroll animation

--- js/script.js
+++ js/script.js
@@ -5,3 +5,23 @@ document.getElementById('focus-link').focus();
 //HIDE HOVER VIDEO TIMELINE UI//
 document.getElementById('me-video').controls = false;
 
+
+
+$(document).ready(function() {
+    // Navigation Scroll //
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+            || location.hostname == this.hostname) {
+    
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+               if (target.length) {
+                 $('html,body').animate({
+                     scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+        });
+});
+
