f7e76842bd1a2f75623807c6e00ba3b77d0e3762
Fu Yen Hsiao <fuyanshaw@gmail.com>
2018-07-16 18:52:49 -0400

    lightbox, work pages, index-about-link

---
+++ js/lightbox.js
@@ -0,0 +1,17 @@
+var $thumbs = $('.thumbnails');
+var $lbImg = $('.lightbox--img');
+var $lb = $('.lightbox');
+
+$thumbs.on('click', 'a', function(link){
+    link.preventDefault();
+    var bigImg = $(this).attr('href');
+    $lbImg.attr('src', bigImg);
+    $lb.attr('data-state', 'visible');  
+})
+
+
+var $btnClose = $('.btn--close');
+
+$btnClose.on('click','svg', function(close) {
+    $lb.attr('data-state', 'hidden');
+})
